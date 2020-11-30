da4b7fc44632361a886041b6a0c82135e86fb252
VadneyK <kentarovadney@berkeley.edu>
2020-11-30 15:02:56 -0800

    start

--- solver.py
+++ solver.py
@@ -13,9 +13,47 @@ def solve(G, s):
         D: Dictionary mapping for student to breakout room r e.g. {0:2, 1:0, 2:1, 3:2}
         k: Number of breakout rooms
     """
+    numOfStudents = len(G)
+    maxHappinessPerK = [0 for i in range(numOfStudents+1)]
 
-    # TODO: your code here!
+    edges = G.edges(data=True)
+    sortedEdges = sorted(edges, key = lambda tuple: tuple[2].get('happiness', 1), reverse = True)
+    
+    for i in range(1, len(maxHappinessPerK)):
+        # assign 
+
+    
+
+
+
     pass
+
+
+def dp_hauffman_solve(G, s):
+    """
+    Args:
+        G: networkx.Graph
+        s: stress_budget
+    Returns:
+        D: Dictionary mapping for student to breakout room r e.g. {0:2, 1:0, 2:1, 3:2}
+        k: Number of breakout rooms
+    """
+    # TODO: your code here!
+    #for e in G.edges:
+    pass
+
+def main():
+    assert len(sys.argv) == 2
+    path = sys.argv[1]
+    G, s = read_input_file(path)
+    D, k = solve(G, s)
+    assert is_valid_solution(D, G, s, k)
+    print("Total Happiness: {}".format(calculate_happiness(D, G)))
+    write_output_file(D, 'out/test.out')
+
+if __name__ = "__main__":
+    main()
+
 
 
 # Here's an example of how to run your solver.
@@ -24,12 +62,12 @@ def solve(G, s):
 
 # if __name__ == '__main__':
 #     assert len(sys.argv) == 2
-#     path = sys.argv[1]
-#     G, s = read_input_file(path)
-#     D, k = solve(G, s)
-#     assert is_valid_solution(D, G, s, k)
-#     print("Total Happiness: {}".format(calculate_happiness(D, G)))
-#     write_output_file(D, 'out/test.out')
+    # path = sys.argv[1]
+    # G, s = read_input_file(path)
+    # D, k = solve(G, s)
+    # assert is_valid_solution(D, G, s, k)
+    # print("Total Happiness: {}".format(calculate_happiness(D, G)))
+    # write_output_file(D, 'out/test.out')
 
 
 # For testing a folder of inputs to create a folder of outputs, you can use glob (need to import it)
