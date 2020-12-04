215108fa2c80135cdea5f954e439d5c1e281bd24
Yaksh Patel <yakshpatel@Yakshs-MacBook-Pro.local>
2020-12-04 14:27:05 -0800

    cleaned up solver.py

--- solver.py
+++ solver.py
@@ -3,9 +3,6 @@ from parse import read_input_file, write_output_file
 from utils import is_valid_solution, calculate_happiness, convert_dictionary, calculate_stress_for_room, calculate_happiness_for_room
 import sys
 
-if __name__ = "__main__":
-    main()
-
 def convertListIntoMap(groupAssignments):
     pairMap = {}
     numOfGroups = 0
@@ -92,32 +89,7 @@ def solve(G, s):
         	bestAssignmentHappiness = currentAssignmentHappiness
         	optimalK = numOfGroups
 
-
     return bestAssignment, optimalK
-
-
-
-def dp_hauffman_solve(G, s):
-    """
-    Args:
-        G: networkx.Graph
-        s: stress_budget
-    Returns:
-        D: Dictionary mapping for student to breakout room r e.g. {0:2, 1:0, 2:1, 3:2}
-        k: Number of breakout rooms
-    """
-    # TODO: your code here!
-    #for e in G.edges:
-    pass
-
-def main():
-    assert len(sys.argv) == 2
-    path = sys.argv[1]
-    G, s = read_input_file(path)
-    D, k = solve(G, s)
-    assert is_valid_solution(D, G, s, k)
-    print("Total Happiness: {}".format(calculate_happiness(D, G)))
-    write_output_file(D, 'out/test.out')
 
 # Here's an example of how to run your solver.
 
@@ -125,21 +97,21 @@ def main():
 
 # if __name__ == '__main__':
 #     assert len(sys.argv) == 2
-    # path = sys.argv[1]
-    # G, s = read_input_file(path)
-    # D, k = solve(G, s)
-    # assert is_valid_solution(D, G, s, k)
-    # print("Total Happiness: {}".format(calculate_happiness(D, G)))
-    # write_output_file(D, 'out/test.out')
+#     path = sys.argv[1]
+#     G, s = read_input_file(path)
+#     D, k = solve(G, s)
+#     assert is_valid_solution(D, G, s, k)
+#     print("Total Happiness: {}".format(calculate_happiness(D, G)))
+#     write_output_file(D, 'outputs/small-1.out')
 
 
 # For testing a folder of inputs to create a folder of outputs, you can use glob (need to import it)
 # if __name__ == '__main__':
-#     inputs = glob.glob('file_path/inputs/*')
+#     inputs = glob.glob('inputs/*')
 #     for input_path in inputs:
-#         output_path = 'file_path/outputs/' + basename(normpath(input_path))[:-3] + '.out'
-#         G, s = read_input_file(input_path, 100)
+#         output_path = 'outputs/' + basename(normpath(input_path))[:-3] + '.out'
+#         G, s = read_input_file(input_path)
 #         D, k = solve(G, s)
 #         assert is_valid_solution(D, G, s, k)
-#         cost_t = calculate_happiness(T)
+#         happiness = calculate_happiness(D, G)
 #         write_output_file(D, output_path)
