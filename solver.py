3b6012855690d5d6937981db488de31dbd8e43d3
Ayush Aggarwal <ayushaggarwal@Ayushs-MacBook-Pro.local>
2020-12-04 21:01:57 -0800

    set up for full testing suite, but still failing large inputs

--- solver.py
+++ solver.py
@@ -2,6 +2,8 @@ import networkx as nx
 from parse import read_input_file, write_output_file
 from utils import is_valid_solution, calculate_happiness, convert_dictionary, calculate_stress_for_room, calculate_happiness_for_room
 import sys
+import glob
+import os
 
 def convertListIntoMap(groupAssignments):
     pairMap = {}
@@ -64,16 +66,16 @@ def solve(G, s):
 
         groupAssignments = [] # list of student sets
 
-        print("K: " + str(i))
+        #print("K: " + str(i))
 
         createdGroups = 0
         maxGroupStress = s / i
 
-        print("Max Group Stress: " + str(maxGroupStress))
+        #print("Max Group Stress: " + str(maxGroupStress))
 
         while createdGroups <= i and len(sortedEdgesCopy) > 0:
         
-            print("Group Assignments: " + str(groupAssignments))
+            #print("Group Assignments: " + str(groupAssignments))
             #print("length of sortedEdges " + str(len(sortedEdgesCopy)))
             mostHappyPair = sortedEdgesCopy.pop(0) #format: (u, v, {happiness: 3, stress: 3})
 
@@ -121,24 +123,25 @@ def solve(G, s):
 
 # Usage: python3 solver.py test.in
 
-if __name__ == '__main__':
-    #assert len(sys.argv) == 2
-    #path = sys.argv[1]
-    path = "50.in"
-    G, s = read_input_file(path)
-    D, k = solve(G, s)
-    assert is_valid_solution(D, G, s, k)
-    print("Total Happiness: {}".format(calculate_happiness(D, G)))
-    write_output_file(D, 'outputs/50.out')
-
-
-# For testing a folder of inputs to create a folder of outputs, you can use glob (need to import it)
 # if __name__ == '__main__':
-#     inputs = glob.glob('inputs/*')
-#     for input_path in inputs:
-#         output_path = 'outputs/' + basename(normpath(input_path))[:-3] + '.out'
-#         G, s = read_input_file(input_path)
-#         D, k = solve(G, s)
-#         assert is_valid_solution(D, G, s, k)
-#         happiness = calculate_happiness(D, G)
-#         write_output_file(D, output_path)
+#     #assert len(sys.argv) == 2
+#     #path = sys.argv[1]
+#     path = "50.in"
+#     G, s = read_input_file(path)
+#     D, k = solve(G, s)
+#     assert is_valid_solution(D, G, s, k)
+#     print("Total Happiness: {}".format(calculate_happiness(D, G)))
+#     write_output_file(D, 'outputs/50.out')
+
+
+#For testing a folder of inputs to create a folder of outputs, you can use glob (need to import it)
+if __name__ == '__main__':
+    inputs = glob.glob('class_inputs/*')
+    for input_path in inputs:
+        print(input_path)
+        output_path = 'class_outputs/' + os.path.basename(input_path)[:-3] + '.out'
+        G, s = read_input_file(input_path)
+        D, k = solve(G, s)
+        assert is_valid_solution(D, G, s, k)
+        happiness = calculate_happiness(D, G)
+        write_output_file(D, output_path)
