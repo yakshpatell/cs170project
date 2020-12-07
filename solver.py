713c608389cbaaadd6148a054bbbd4a69eb3accb
Ayush Aggarwal <ayushaggarwal@Ayushs-MacBook-Pro.local>
2020-12-06 18:21:56 -0800

    all people assigned

--- solver.py
+++ solver.py
@@ -109,7 +109,7 @@ def solve(G, s):
 
         groupMap, numOfGroups = convertListIntoMap(groupAssignments)
         currentAssignmentHappiness = calculate_happiness(groupMap, G)
-        if currentAssignmentHappiness > bestAssignmentHappiness:
+        if currentAssignmentHappiness > bestAssignmentHappiness and len(groupMap) == len(G):
             bestAssignment = groupMap
             bestAssignmentHappiness = currentAssignmentHappiness
             optimalK = numOfGroups
@@ -125,17 +125,17 @@ def solve(G, s):
 # if __name__ == '__main__':
 #     #assert len(sys.argv) == 2
 #     #path = sys.argv[1]
-#     path = "50.in"
+#     path = "inputs/small-234.in"
 #     G, s = read_input_file(path)
 #     D, k = solve(G, s)
 #     assert is_valid_solution(D, G, s, k)
 #     print("Total Happiness: {}".format(calculate_happiness(D, G)))
-#     write_output_file(D, '50.out')
+#     write_output_file(D, 'small-234.out')
 
 
-#For testing a folder of inputs to create a folder of outputs, you can use glob (need to import it)
+# For testing a folder of inputs to create a folder of outputs, you can use glob (need to import it)
 if __name__ == '__main__':
-    inputs = glob.glob('class_inputs/*')
+    inputs = glob.glob('inputs/*')
     for input_path in inputs:
         print(input_path)
         output_path = 'class_outputs/' + os.path.basename(input_path)[:-3] + '.out'
